d86c773490c4cef8b7d9d9d0747f27a50b17eaf2
Mehmet Öztürk <59453560+mehmetztrk21@users.noreply.github.com>
2022-04-16 11:13:24 +0300

    Adding Last 5 Blog View Component for Writer Dashboard.

---
+++ CoreDemo/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents.Blog
+{
+    public class BlogListDashboard:ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = bm.GetBlogsWithCategoryName();
+            return View(values);
+        }
+    }
+}
